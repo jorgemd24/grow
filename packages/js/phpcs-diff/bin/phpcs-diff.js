c0c24273e5be5d118472b18bbd288d0439e38cc0
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2022-07-22 18:15:29 +0400

    Execute node_modules sh

--- packages/js/phpcs-diff/bin/phpcs-diff.js
+++ packages/js/phpcs-diff/bin/phpcs-diff.js
@@ -8,4 +8,4 @@ import path from 'path';
 process.env.PATH +=
 	path.delimiter + path.join( process.cwd(), 'node_modules', '.bin' );
 
-shell.exec( 'sh phpcs-diff.sh' );
+shell.exec( 'node_modules/woocommerce-grow-phpcs-diff/bin/phpcs-diff.sh' );
